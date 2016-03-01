fdd832f67e8a78e3785a1ac263a97b1a92709417
Dakota Martinez <dakotaleemusic@gmail.com>
2016-03-01 23:19:43 +0000

    adds index and show views for students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,10 +1,38 @@
 class StudentsController < ApplicationController
   get "/students" do 
-    
+    logged_in_do? do
+      has_permission?(is_admin? || is_teacher? || is_parent?) do
+        @students = Student.all if is_admin?
+        @students = current_teacher.students if is_teacher?
+        @students = current_parent.students if is_parent?
+        erb :"/students/index" if @students
+      end
+    end
   end
   
   get "/students/:id" do 
-    @student = Student.find(params[:id])
-    erb :"/students/show"
+    logged_in_do? do
+      id = params[:id].to_i
+      has_permission?(is_student? && current_student.id == id || is_teacher? && current_teacher.students.include?(Student.find(id)) || is_admin?) do 
+        @student = Student.find(params[:id])
+        load_times
+        erb :"/students/show"
+      end
+    end
   end
+  
+  get "/students/:id/:date" do 
+    logged_in_do? do 
+      id = params[:id].to_i
+      has_permission?(is_student? && current_student.id == id || is_teacher? && current_teacher.students.include?(Student.find(id)) || is_admin?) do 
+        @student = Student.find(params[:id])
+        @date = DateTime.parse(params[:date])
+        load_times
+        erb :"/students/calendar"
+      end
+    end
+  end
+
 end
+
+    
