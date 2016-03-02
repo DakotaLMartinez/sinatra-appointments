5c2e24f8950afdb0793c8f85a1a3003c1bf149b9
Dakota Martinez <dakotaleemusic@gmail.com>
2016-03-02 00:11:54 +0000

    adds index and show views for parents

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -1,12 +1,28 @@
 class ParentsController < ApplicationController
   
   get "/parents" do 
-    
+    logged_in_do? do
+      has_permission?(is_admin? || is_teacher?) do
+        is_admin? ? @parents = Parent.all : nil
+        is_teacher? ? @parents = current_teacher.parents : nil
+        erb :"/parents/index"
+      end
+    end
   end
   
-  get "/parents/:id" do 
-    @parent = Parent.find(params[:id])
-    erb :"/parents/show"
+  get "/parents/:id" do
+    logged_in_do? do 
+      has_permission?(is_admin? || is_teacher? || is_parent?) do 
+        is_admin? ? @parent = Parent.find(params[:id].to_i) : nil
+        has_permission?( is_teacher? && current_teacher.parents.include?(Parent.find(params[:id])) ) do
+          @parent = Parent.find(params[:id])
+        end
+        has_permission?(is_parent? && current_parent.id == params[:id].to_i) do
+          @parent = Parent.find(params[:id])
+        end
+        erb :"/parents/show"
+      end
+    end
   end
     
 end
