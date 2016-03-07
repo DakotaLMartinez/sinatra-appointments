03e0a7d800e95a8ff205fc05e516e39a0357a533
Dakota Martinez <dakotaleemusic@gmail.com>
2016-03-07 01:20:26 +0000

    changes booking_request edit form to lessons edit form on teacher show page

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -10,15 +10,15 @@ class TeachersController < ApplicationController
     logged_in_do? do
       if is_student?
         @students = [current_student]
-        @booking_requests = current_student.booking_requests.select { |br| br.teacher_id == params[:id] }
+        @lessons = current_student.lessons.select { |l| l.teacher_id == params[:id] }
       end
       if is_teacher?
         @students = current_teacher.students
-        @booking_requests = current_teacher.booking_requests
+        @lessons = current_teacher.lessons
       end
       if is_parent?
         @students = current_parent.students
-        @booking_requests = current_parent.booking_requests.select { |br| br.teacher_id == params[:id] }
+        @lessons = current_parent.lessons.select { |l| l.teacher_id == params[:id] }
       end
       
       @teachers = [@teacher]
@@ -30,9 +30,9 @@ class TeachersController < ApplicationController
     erb :"teachers/show"
   end
   
-  post "/teachers/:id/booking_requests" do 
+  post "/teachers/:id/lessons" do 
     logged_in_do? do 
-      @booking_request = BookingRequest.find(params[:booking_request])
+      @lesson = Lesson.find(params[:lesson])
       @teacher = Teacher.find(params[:id])
       
       @students = current_teacher.students if is_teacher?
@@ -47,39 +47,39 @@ class TeachersController < ApplicationController
     end
   end
   
-  post "/teachers/:id/booking_requests/:br_id" do 
+  post "/teachers/:id/lessons/:lesson_id" do 
     logged_in_do? do
       # checks for proper permission before proceeding with the update
-      has_permission?(is_teacher? && current_teacher.booking_requests.include?(BookingRequest.find(params[:br_id])) || is_parent? && current_parent.booking_requests.include?(BookingRequest.find(params[:br_id])) || is_student? && current_student.booking_requests.include?(BookingRequest.find(params[:br_id])) || is_admin?) do
-        @booking_request = BookingRequest.find(params[:br_id])
-        student = Student.find(params[:booking_request][:student].to_i)
-        teacher = Teacher.find(params[:booking_request][:teacher].to_i)
-        lesson_time = DateTime.parse(params[:booking_request][:lesson_time][:date] + " #{params[:booking_request][:lesson_time][:hour]}:#{params[:booking_request][:lesson_time][:minutes]}")
-        location = Location.find(params[:booking_request][:location].to_i)
+      has_permission?(is_teacher? && current_teacher.lessons.include?(Lesson.find(params[:lesson_id])) || is_parent? && current_parent.lessons.include?(Lesson.find(params[:lesson_id])) || is_student? && current_student.lessons.include?(Lesson.find(params[:lesson_id])) || is_admin?) do
+        @lesson = Lesson.find(params[:lesson_id])
+        student = Student.find(params[:lesson][:student].to_i)
+        teacher = Teacher.find(params[:lesson][:teacher].to_i)
+        lesson_time = DateTime.parse(params[:lesson][:lesson_time][:date] + " #{params[:lesson][:lesson_time][:hour]}:#{params[:lesson][:lesson_time][:minutes]}")
+        location = Location.find(params[:lesson][:location].to_i)
         # makes sure objects are valid before saving
         if !student.is_a?(Student)
-          erb :"booking_requests/edit", locals: { message: "Couldn't find that student." }
+          erb :"lessons/edit", locals: { message: "Couldn't find that student." }
         elsif !teacher.is_a?(Teacher)
-          erb :"booking_requests/edit", locals: { message: "Couldn't find that teacher." }
+          erb :"lessons/edit", locals: { message: "Couldn't find that teacher." }
         elsif !lesson_time.is_a?(DateTime) || !validate_time(lesson_time)
-          erb :"booking_requests/edit", locals: { message: "Please select a valid time" }
+          erb :"lessons/edit", locals: { message: "Please select a valid time" }
         elsif !location.is_a?(Location)
-          erb :"booking_requests/edit", locals: { message: "Couldn't find that location" }
+          erb :"lessons/edit", locals: { message: "Couldn't find that location" }
         end
         
         if is_teacher?
           # only allows the teacher to edit their own booking requests
-          if @booking_request.teacher == current_teacher
-            @booking_request.teacher = teacher
+          if @lesson.teacher == current_teacher
+            @lesson.teacher = teacher
           else
             erb :failure, locals: { message: "You don't have permission to edit this booking request" }
           end
           # prevents teachers from chaning a booking request to belong to another student
-          if @booking_request.student != student
-            erb :"booking_requests/edit", locals: { message: "Unable to change student." }
+          if @lesson.student != student
+            erb :"lessons/edit", locals: { message: "Unable to change student." }
           end
-          @booking_request.lesson_time = lesson_time
-          @booking_request.location = location
+          @lesson.lesson_time = lesson_time
+          @lesson.location = location
         end
         
         if is_parent?
@@ -87,17 +87,17 @@ class TeachersController < ApplicationController
         end
         
         if is_admin?
-          @booking_request.student = student
-          @booking_request.teacher = teacher
-          @booking_request.lesson_time = lesson_time
-          @booking_request.location = location
+          @lesson.student = student
+          @lesson.teacher = teacher
+          @lesson.lesson_time = lesson_time
+          @lesson.location = location
         end
         
         if is_student?
           
         end
         
-        if @booking_request.save
+        if @lesson.save
           redirect "/teachers/#{teacher.id}"
         end
         
