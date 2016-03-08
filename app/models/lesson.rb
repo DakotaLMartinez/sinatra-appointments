74812ff6e95098ba8b17caf1ff36da9c93c300d0
Dakota Martinez <dakotaleemusic@gmail.com>
2016-03-08 20:47:35 +0000

    adds validations to lesson and student models

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,5 +1,41 @@
 class Lesson < ActiveRecord::Base
+  include ActiveModel::Validations
   belongs_to :student
   belongs_to :teacher
   belongs_to :location
+  
+  # validates_associated :student, :teacher, :location
+  validates_numericality_of :duration, only_integer: true
+  validates_inclusion_of :duration, in: [1800, 2700, 3600, 4500, 5400]
+  validate do |lesson|
+    LessonTimeValidator.new(lesson).validate  
+  end
+end
+
+class LessonTimeValidator
+  def initialize(lesson)
+    @lesson = lesson
+    @teacher = @lesson.teacher
+  end
+  
+  def validate
+    validate_lesson_time
+    
+  end
+  
+  def validate_lesson_time
+    # stores today's lessons
+    lessons = @teacher.lessons.select { |l| @lesson.lesson_time.midnight == l.lesson_time.midnight }
+    # makes sure today's lessons don't overlap with the new lesson
+    # first checks is another lesson is still in progress when the new lesson is set to start.
+    # next checks if the new lesson would be still in progress when any of the other lessons are set to start.
+    lessons.each do |lesson|
+      if @lesson != lesson
+        if lesson.lesson_time < @lesson.lesson_time && @lesson.lesson_time < lesson.lesson_time + lesson.duration.seconds || @lesson.lesson_time < lesson.lesson_time && lesson.lesson_time < @lesson.lesson_time + @lesson.duration.seconds
+          @lesson.errors.add(:lesson_time, "is not available.")
+        end
+      end
+    end
+  end
+  
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -6,4 +6,9 @@ class Student < ActiveRecord::Base
   has_many :parent_students
   has_many :parents, -> { uniq }, through: :parent_students
   belongs_to :user
+  
+  validates_presence_of :name
+  validates :email, format: { with: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i, message: 'Please enter a valid email address'}, allow_blank: true
+  validates :phone_number, format: { with: /\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/, message: "Please enter a valid phone number (123) 456-7890" }, allow_blank: true
+  validates_inclusion_of :contact_method, in: ["phone", "email"], allow_nil: true
 end
