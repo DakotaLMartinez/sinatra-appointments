dbb64222d9e129c3f9571fd0777d11e0aabd1ade
Dakota Martinez <dakotaleemusic@gmail.com>
2016-03-11 18:45:08 +0000

    fixes roles model spec

--- spec/models/02_role_spec.rb
+++ spec/models/02_role_spec.rb
@@ -1,9 +1,9 @@
 require "spec_helper"
 
 describe Role do 
-  let(:admin) { Role.find_or_create_by(name: "supreme leader") }
-  let(:sandra) { User.find_or_create_by(username: "sandra", password: "test123", role_id: admin.id) } 
-  let(:dakota) { User.find_or_create_by(username: "dakota", password: "test123", role_id: admin.id) }
+  let(:admin) { Role.create(name: "supreme leader") }
+  let(:sandra) { User.create(username: "sandra", password: "test123", role_id: admin.id) } 
+  let(:dakota) { User.create(username: "dakota", password: "test123", role_id: admin.id) }
   it "has many users" do
     expect(admin.users).to include(sandra, dakota)
   end
