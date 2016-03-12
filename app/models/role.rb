eddf12434691bbe7bb5f24dbaaa28d521c4c977e
Dakota Martinez <dakotaleemusic@gmail.com>
2016-03-12 02:36:10 +0000

    adds validations to user roles

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,6 @@
 class Role < ActiveRecord::Base 
   has_many :users  
+  valid_role_names = ["student", "parent", "teacher", "supreme leader"]
+  validates :name, presence: true, uniqueness: true
+  validates_inclusion_of :name, in: valid_role_names
 end
--- spec/models/02_role_spec.rb
+++ spec/models/02_role_spec.rb
@@ -1,10 +1,26 @@
 require "spec_helper"
 
 describe Role do 
-  let(:admin) { Role.create(name: "supreme leader") }
+  let(:admin) { Role.find_or_create_by(name: "supreme leader") }
   let(:sandra) { User.create(username: "sandra", password: "test123", role_id: admin.id) } 
   let(:dakota) { User.create(username: "dakota", password: "test123", role_id: admin.id) }
+  
   it "has many users" do
     expect(admin.users).to include(sandra, dakota)
   end
+  
+  context "validations" do 
+    
+    it "only accepts valid user roles (student, parent, teacher or supreme leader)" do 
+      astronaut = Role.new(name: "astronaut!")
+      expect(astronaut.valid?).to be_falsey
+    end
+    
+    it "maintains uniqueness of user role names" do 
+      admin2 = Role.new(name: "supreme leader")
+      expect(admin2.valid?).to be_falsey
+    end
+    
+  end
+  
 end
